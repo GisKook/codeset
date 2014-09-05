ac9ddc5e3305b67c9b3788c4016123d7759561f1
GisKook <zhangkai.gis@163.com>
2014-09-04 21:04:20 -0700

    tobe parse the protocol

--- toolkit.h
+++ toolkit.h
@@ -13,4 +13,7 @@ void debug_printbytes(unsigned char* buf, unsigned int len);
 
 void hex2char(char* charbuf, unsigned char* hexbuf, unsigned int len);
 
+#define TOOLKIT_BYTE2INT(x) ((*x*256*256*256)+(*(x+1)*256*256)+(*(x+2)*256)+(*(x+3)))
+#define TOOLKIT_BYTE2SHORT(x) ((*x*256)+(*(x+1)))
+
 #endif
