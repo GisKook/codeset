69de45d0e3f2a5eb46c502b4030c0da26f294676
GisKook <zhangkai.gis@163.com>
2014-11-24 00:02:00 -0800

    add a macro.

--- rbtree/rbtree.h
+++ rbtree/rbtree.h
@@ -97,6 +97,7 @@ static inline struct page * rb_insert_page_cache(struct inode * inode,
 #include <stdlib.h>
 
 #ifdef __linux__
+#define off_setof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 #define container_of(ptr, type, member) ({          \
 	const typeof(((type *)0)->member)*__mptr = (ptr);    \
 	(type *)((char *)__mptr - off_setof(type, member)); })
