77051827200198aff2cf2e29d2d0b7763dbdbba5
giskook <zhangkai.gis@163.com>
2015-02-06 10:41:01 +0800

    to do it.

--- netlistflattener/netlistflattener/edif.c
+++ netlistflattener/netlistflattener/edif.c
@@ -8,7 +8,8 @@ int edifgetcellcount(struct edifcell * edifcell){
 	for(cell = edifcell, count = 0; cell != NULL; cell=cell->next, ++count);
 	return count;
 }
-char ** edifgetcellname(struct edifcell * edifcell){ 
+
+char ** edifgetcellname(struct edifcell * edifcell){
 	int count = edifgetcellcount(edifcell);
 	struct edifcell *cell;
 	char ** cellname = (char **)malloc(sizeof(char *)*(count+1));
@@ -21,13 +22,49 @@ char ** edifgetcellname(struct edifcell * edifcell){
 	return cellname;
 }
 
-struct edifcell * edifflatten(struct edifcell * edifcells){ 
+int edifgetcellinstancecount(struct edifcell * edficell){
+	struct edifinstance * edifinstance = NULL, *instance = NULL;
+	int count = 0;
+	if(edficell != NULL && edifcell->edifcontents != NULL && edifcell->edifcontents->edifinstance != NULL){ 
+		edifinstance = edficell->edifcontents->edifinstance;
+		for(instance = edifinstance ; instance != NULL; instance = edifinstance->next, ++count);
+	} 
+
+	return count;
+}
+
+struct edifinstance * edifgetcellinstances(struct edifcell * edifcell){
+	if(edifcell != NULL && edifcell->edifcontents != NULL && edifcell->edifcontents->edifinstance != NULL){ 
+		return edifcell->edifcontents->edifinstance;
+	}
+}
+
+struct edifcell * edifcellcopy(struct edifcell * edifcell){ 
+	struct edifcell * cell = (struct edifcell *)malloc(sizeof(struct edifcell));
+	cell->cell = strdup(edifcell->cell);
+	cell->celltype = strdup(edifcell->celltype);
+
+}
+
+struct edifcell * edifcellflatten(struct edifcell * edifcellout, struct edifcell * edifcellin){ 
+	int i;
+	if (edifcellin->edifinterfaceport != NULL) { 
+	}
+}
+
+struct edifcell * ediflibraryflatten(struct edifcell * edifcells){
 	struct edifcell * cell = (struct edifcell *)malloc(sizeof(struct edifcell));
 	char ** cellname ;
+	int cellcount = 0;
+	int i;
 	memset(cell, 0, sizeof(struct edifcell));
 	cell->cell = strdup(edifcells->cell);
 	cell->celltype = strdup(edifcells->celltype);
-	cellname = edifgetcellname(edifcells);
+	cellname = edifgetcellname(edifcells); 
+	cellcount = edifgetcellcount(edifcells); 
+	for (i = 0; i < cellcount; ++i) {
+
+	}
 
 	free(cellname);
 
