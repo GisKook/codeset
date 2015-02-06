69767a3ba67422fc4be3c5f4be90b8e3abb2a8f3
giskook <zhangkai.gis@163.com>
2015-02-06 14:57:59 +0800

    miss library

---
+++ netlistflattener/netlistflattener/ediflibrary.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "edif.h"
+
+struct ediflibrary * ediflibrary_flatten(struct ediflibrary * ediflibrarys){
+
+}
+
+void ediflibrary_writer(struct ediflibrary * ediflibrary, FILE * out){
+
+}
