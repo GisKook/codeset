7e153b8b3783b09b86886413c4ae8a1a5f1eda55
giskook <zhangkai.gis@163.com>
2015-02-10 08:25:24 +0800

    forget to commit

---
+++ netlistflattener/netlistflattener/edifinstance.h
@@ -0,0 +1,10 @@
+#ifndef EDIFINSTANCE_H_H
+#define EDIFINSTANCE_H_H
+
+struct edifinstance;
+struct edifsubcircuit;
+struct ediflibrary;
+struct edifinstance * edifinstance_flatten(struct edifinstance * instance, struct ediflibrary * library, struct edifsubcircuit * subcircuit); 
+struct edifinstance * edifinstance_copy(struct edifinstance * edifinstance);
+
+#endif
