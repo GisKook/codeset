adffdf073efe33084213d08c2ef79f65aafa3384
unknown <Gis_Kook@.(none)>
2015-03-07 06:27:56 +0800

    save dlg

--- netlistflattener/edifnetlistflattener/edifnetlistflattenerDlg.cpp
+++ netlistflattener/edifnetlistflattener/edifnetlistflattenerDlg.cpp
@@ -212,7 +212,7 @@ void CedifnetlistflattenerDlg::OnBnClickedButtonOutfile()
 	TCHAR szFilters[]= _T("EDF Files (*.edf)|*edf|EDN Files (*.edn)|*.edn|All Files (*.*)|*.*||");
 	CFileDialog fileDlg(FALSE, _T(""), _T("*.edf"),
 		OFN_FILEMUSTEXIST | OFN_HIDEREADONLY, szFilters);
-	fileDlg.GetOFN().lpstrFileTitle = "save netlist file";
+	fileDlg.GetOFN().lpstrTitle = "save netlist file";
 	if(fileDlg.DoModal() == IDOK)
 	{
 		m_strOutFileName = fileDlg.GetPathName();
